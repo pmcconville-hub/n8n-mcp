cd9f112605e408b2b70f115be12e8992c4dc597b
github-action-benchmark <github@users.noreply.github.com>
2025-11-29 02:26:21 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 2f5a8571427a6ce3dc614c5435f61925edebb376

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1764383181960,
+  "repoUrl": "https://github.com/pmcconville-hub/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "devangkantharia@gmail.com",
+            "name": "devangkantharia",
+            "username": "devangkantharia"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2f5a8571427a6ce3dc614c5435f61925edebb376",
+          "message": "Added Antigravity Setup Instructions (#452)\n\n* Add Antigravity setup documentation\n\nDocument the setup process for Antigravity with n8n MCP server, including preconditions, installation steps, configuration, and best practices.\n\n* Add Antigravity integration guide to README\n\nAdded a new section for Antigravity integration.",
+          "timestamp": "2025-11-29T00:56:12+01:00",
+          "tree_id": "d1cc0c90e0cdbfefde9f4021b0293f4c75643384",
+          "url": "https://github.com/pmcconville-hub/n8n-mcp/commit/2f5a8571427a6ce3dc614c5435f61925edebb376"
+        },
+        "date": 1764383181259,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
